d3d16ee659f660e83f33d20a6f506020189929a9
Brian Pick <brianr.pick@gmail.com>
2016-11-09 20:08:33 -0800

    Added dynamic page headings to each page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,18 @@
 class ItemsController < ApplicationController
   def home
-
+    @page_heading = "Welcome to wesellallot.com, Heres what we have for sale!"
   end
 
   def chair
+    @page_heading = "Heres the chair we sell"
   end
 
   def hat
+    @page_heading = "Heres the hat we sell"
   end
 
   def pants
+    @page_heading = "Heres the pants that we sell"
   end
-  
+
 end
