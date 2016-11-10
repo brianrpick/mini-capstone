6be7bfc80354f4eb3d0116aad6ed1f2e57c0eb8c
Brian Pick <brianr.pick@gmail.com>
2016-11-09 19:56:15 -0800

    added bootstrap, created initial view for items and a home page, added routs to each view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/", to: "items#home"
+  get "/chair", to: "items#chair"
+  get "/hat", to: "items#chair"
+  get "/pants", to: "items#pants"
+
 end
+
