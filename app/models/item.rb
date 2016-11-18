4da7864e873cb97f182451ede9357f9b8cd8ac2e
Brian Pick <brianr.pick@gmail.com>
2016-11-17 19:53:28 -0800

    Added tax, total with tax, and on sale message, not finished with adding pictures quite yet

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,21 @@ class Item < ApplicationRecord
   def pretty_time
     return created_at.strftime("%a, %r %b %y")
   end
+
+  def sale_message
+    if price.to_i < 2
+      return "On Sale!"
+    else 
+      return "Regular Price"
+    end
+  end
+
+  def tax
+    return price.to_i * 0.09
+  end
+
+  def total 
+    return price.to_i + (price.to_i * 0.09)
+  end
+
 end
